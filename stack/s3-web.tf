df82c2f43aeca118144167c0d75a1dddc9f391ff
John <29466887+ockendenjo@users.noreply.github.com>
2026-06-23 11:59:42 +0100

    Add KMS key and S3 bucket for static web files

---
+++ stack/s3-web.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "static_web" {
+  bucket_prefix = "snooker-${var.env}-"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "static_web" {
+  bucket = aws_s3_bucket.static_web.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.main.arn
+    }
+    bucket_key_enabled = true
+  }
+}
