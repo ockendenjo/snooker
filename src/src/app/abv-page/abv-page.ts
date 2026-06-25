d7a8b59c4e625b6853aca05d148d0823d208077a
John <29466887+ockendenjo@users.noreply.github.com>
2026-06-25 16:36:28 +0100

    Add page with ABV table

---
+++ src/src/app/abv-page/abv-page.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "app-abv-page",
+    imports: [],
+    templateUrl: "./abv-page.html",
+    styleUrl: "./abv-page.scss",
+})
+export class AbvPage {}
