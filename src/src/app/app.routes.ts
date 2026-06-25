78ee9be97a620349cf827c9701e5d0c4692170a0
John Ockenden <29466887+ockendenjo@users.noreply.github.com>
2026-06-25 16:39:00 +0100

    Add page with ABV table (#9)

--- src/src/app/app.routes.ts
+++ src/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import {Routes} from "@angular/router";
 import {HomePage} from "./home-page/home-page";
+import {AbvPage} from "./abv-page/abv-page";
 
 export const routes: Routes = [
     {
         path: "",
         component: HomePage,
+    },
+    {
+        path: "abvs",
+        component: AbvPage,
     },
 ];
