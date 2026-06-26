f790d2ba41d61b0d214eeb20b3cc55ccb4cf45af
John Ockenden <29466887+ockendenjo@users.noreply.github.com>
2026-06-26 14:35:26 +0100

    Add info page (#12)

---
+++ src/src/app/info-page/info-page.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "app-info-page",
+    imports: [],
+    templateUrl: "./info-page.html",
+    styleUrl: "./info-page.scss",
+})
+export class InfoPage {}
