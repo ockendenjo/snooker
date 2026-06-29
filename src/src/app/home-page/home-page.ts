7f965713264863e8cdee5467fdee56ff8ca36fc3
John <29466887+ockendenjo@users.noreply.github.com>
2026-06-29 17:08:58 +0100

    Add sign-in button to home-page

--- src/src/app/home-page/home-page.ts
+++ src/src/app/home-page/home-page.ts
@@ -1,4 +1,7 @@
 import {Component} from "@angular/core";
+import {toSignal} from "@angular/core/rxjs-interop";
+import {AuthState, SessionService} from "../services/session.service";
+import {AuthService} from "../services/auth.service";
 
 @Component({
     selector: "app-home-page",
@@ -6,4 +9,21 @@ import {Component} from "@angular/core";
     templateUrl: "./home-page.html",
     styleUrl: "./home-page.scss",
 })
-export class HomePage {}
+export class HomePage {
+    public readonly authData;
+
+    constructor(
+        private authService: AuthService,
+        private readonly sessionSvc: SessionService,
+    ) {
+        this.authData = toSignal(this.sessionSvc.getAuthDataObservable(), {
+            requireSync: true,
+        });
+    }
+
+    public signIn(): void {
+        this.authService.login();
+    }
+
+    protected readonly AuthState = AuthState;
+}
