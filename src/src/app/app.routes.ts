14e792348dba8d2c6b13ddd0395e7e919b53f95a
John <29466887+ockendenjo@users.noreply.github.com>
2026-07-01 13:14:57 +0100

    more work

--- src/src/app/app.routes.ts
+++ src/src/app/app.routes.ts
@@ -5,7 +5,6 @@ import {AbvPage} from "./abv-page/abv-page";
 import {EdiMapPage} from "./edi-map-page/edi-map-page";
 import {CallbackComponent} from "./callback/callback.component";
 import {ProfilePage} from "./profile-page/profile-page";
-import {AuthService} from "./services/auth.service";
 import {requireSignedIn} from "./auth.guards";
 
 export const routes: Routes = [
